91435f8cacc284251458c29d81b2dd842beb4f40
Michael Overstreet <FireGoblin21@gmail.com>
2012-09-13 04:25:52 -0400

    Tweaked constants.

--- Gamification/Constants.m
+++ Gamification/Constants.m
@@ -11,25 +11,25 @@
 //GoalSet
 const int kExpGain[6] = {1, 2, 4, 12, 36, 144};
 
-const double kCommonChance[6] = {.01, .1, .5, 1, 1, 1};
-const double kUncommonChance[6] = {.001, .01, .05, .25, 1, 1};
-const double kRareChance[6] = {.0001, .001, .005, .025, .125, 1};
-const double kEpicChance[6] = {.00001, .0001, .0005, .0025, .0125, .125};
-
+const double kCommonChance[6] =     {.0625, .125,   .25,    1,      1,    1};
+const double kUncommonChance[6] =   {.016,  .04,    .1,     .25,    1,    1};
+const double kRareChance[6] =       {0,     .002,   .01,    .05,   .25,   1};
+const double kEpicChance[6] =       {0,     0,      .00025, .0025, .025,  .25};
+   
 
 //UserStatus
-const NSTimeInterval kExpirationTime = 1296000;  //15 days
+const NSTimeInterval kExpirationTime = 1000000;  //11 days, 13 hours, and 47 minutes
 const int kMaxCount = 10;
 
 //10x + 90 increase with each level
 const int kExpToLevelMap[51] =
 {0,     100,    210,    330,    460,    600,    750,    910,    1080,   1260,   1450,
-    1650,   1860,   2080,   2310,   2550,   2800,   3060,   3330,   2610,   2900,
-    3200,   3510,   3830,   4160,   4500,   4850,   5210,   5580,   5960,   6350,
-    6750,   7160,   7580,   8010,   8450,   8900,   9360,   9830,   10310,  10800,
-    11300,  11810,  12330,  12860,  13400,  13950,  14510,  15080,  15660,  16250};
+    1650,   1860,   2080,   2310,   2550,   2800,   3060,   3330,   3610,   3900,
+    4200,   4510,   4830,   5160,   5500,   5850,   6210,   6580,   6960,   7350,
+    7750,   8160,   8580,   9010,   9450,   9900,   10360,  10830,  11310,  11800,
+    12300,  12810,  13330,  13860,  14400,  14950,  15510,  16080,  16660,  17250};
 const int kMaxLevel = 50;
 
 
-double const kStackValue = 0.05;
-double const kLevelValue = 0.01;
+double const kStackValue = 0.075;
+double const kLevelValue = 0.015;
